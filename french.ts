4a3825080a3f1af581430504d3396e255a2379b8
ShenYoup0ng <89461600+ShenYoup0ng@users.noreply.github.com>
2021-11-02 17:34:46 +0100

    Update french.ts

--- french.ts
+++ french.ts
@@ -10,7 +10,7 @@ Mode de jeu actuel : @{gamemode[0]}
 Abonnez-vous à notre Patreon https://www.patreon.com/skeld_net pour obtenir un nom coloré dans le jeu !`.trim(),
     SetImpostorsInvalidCount:
         "Nombre de joueurs invalide, merci d'entrer un nombre compris entre 1 et 3",
-    SetImpostorsSuccess: "Le nombre d'imposteur a été mis à @{number[0]}",
+    SetImpostorsSuccess: "Le nombre d'imposteurs a été mis à @{number[0]}",
     ZombiesIncomingIn: "Les zombies arrivent dans @{number[0]} secondes",
     GamemodeChangedTo: "Mode de jeu changé en @{gamemode[0]}",
     SquidGameLightsInstructions:
